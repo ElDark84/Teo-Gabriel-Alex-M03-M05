67f4aefb4ea754f0298799aae93ea92c001867a5
alexJimenez <alex.jimenez.estudios@gmail.com>
2024-05-09 21:16:08 +0200

    Librerias añadidas
    Librerias añadidas al main

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Random;//Para poder generar numeros aleatorios
+import java.util.Scanner;// Para poder generar un scanner
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
