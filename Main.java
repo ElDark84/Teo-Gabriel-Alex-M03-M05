f951601b91f67b50d1fcef5f0c627cad01268a0b
alexJimenez <alex.jimenez.estudios@gmail.com>
2024-05-14 20:38:28 +0200

    Clase Abstracta Libro

--- Main.java
+++ Main.java
@@ -3,6 +3,29 @@ import java.io.*;               //Liberia para uso, lectura y escritura en archi
 import java.util.ArrayList;     //Liberia para uso de Arrays y ArrayList
 import java.util.Scanner;       //Librerai para uso del Scanner
 
+//Clase Abstracta Libro
+abstract class Libro {
+    private String titulo;
+    private String autor;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    // Método abstracto para calcular el precio del libro
+    public abstract double calcularPrecio();
+}
+
+
 public class Main {
     public static void main(String[] args) {
 
