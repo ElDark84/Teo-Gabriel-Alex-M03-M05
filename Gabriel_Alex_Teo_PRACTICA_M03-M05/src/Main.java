b5407d6abced6f77de3d03f8e328a8e9b89dc23c
teoaranda <157788932+teoaranda@users.noreply.github.com>
2024-05-15 01:24:35 +0200

    Added class libro

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
@@ -1,10 +1,9 @@
-//Bibliotecas
 import java.io.*;               //Liberia para uso, lectura y escritura en archivos
 import java.util.ArrayList;     //Liberia para uso de Arrays y ArrayList
-import java.util.Scanner;       //Librerai para uso del Scanner
+import java.util.Scanner;   
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        
     }
 }
