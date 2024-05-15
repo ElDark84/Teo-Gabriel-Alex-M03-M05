d7a39e29abf930861d45eb6b969fca1d4fe6934e
alexJimenez <alex.jimenez.estudios@gmail.com>
2024-05-15 10:00:03 +0200

    Comentario Libreria Scanner

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;               //Liberia para uso, lectura y escritura en archivos
 import java.util.ArrayList;     //Liberia para uso de Arrays y ArrayList
-import java.util.Scanner;   
+import java.util.Scanner;       //Liberia para uso de Scanner
 
 public class Main {
     public static void main(String[] args) throws Exception {
