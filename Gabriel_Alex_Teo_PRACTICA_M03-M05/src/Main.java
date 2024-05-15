b3c6976f7f934c79812f577e35420bd43c78ea7e
teoaranda <157788932+teoaranda@users.noreply.github.com>
2024-05-15 12:11:03 +0200

    Update Main.java

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
@@ -36,5 +36,30 @@ public class Main {
         System.out.println("Libro no encontrado en la biblioteca.");
     }
 
+    public void leerEntradaPorConsola() { //GIT ALEX
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nIntroduce tu nombre: ");
+        String nombreUsuario = scanner.nextLine();
+        System.out.println("¡Hola, " + nombreUsuario + "!");
+        
+        // Escritura de archivo de texto en una carpeta específica
+        try {
+            File carpeta = new File("prestamos");
+            if (!carpeta.exists()) {
+                carpeta.mkdir();
+            }
+
+            FileWriter archivoSalida = new FileWriter("prestamos/prestamos.txt", true);
+            BufferedWriter escritor = new BufferedWriter(archivoSalida);
+            escritor.newLine();
+            escritor.write("Usuario: " + nombreUsuario);
+            escritor.write(" - Libro prestado: " + libroSeleccionado.getTitulo());
+            escritor.close();
+            System.out.println("\nInformación de préstamo actualizada en prestamos/prestamos.txt");
+        } catch (IOException e) {
+            System.err.println("\nError al escribir el archivo: " + e.getMessage());
+        }
+    }
+
 }
 
