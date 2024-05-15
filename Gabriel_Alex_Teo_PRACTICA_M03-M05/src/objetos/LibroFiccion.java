1a27e881262135c2d402126089d51a3139a89d99
ElDark84 <contactodark84@gmail.com>
2024-05-15 10:03:23 +0200

    Create LibroFiccion.java

---
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/objetos/LibroFiccion.java
@@ -0,0 +1,14 @@
+public class LibroFiccion extends Libro {
+    private int paginas;
+
+    public LibroFiccion(String titulo, String autor, int paginas) {
+        super(titulo, autor);
+        this.paginas = paginas;
+    }
+
+    @Override
+    public double calcularPrecio() {
+        // Precio base para libros de ficción
+        return paginas * 0.1;
+    }
+}
