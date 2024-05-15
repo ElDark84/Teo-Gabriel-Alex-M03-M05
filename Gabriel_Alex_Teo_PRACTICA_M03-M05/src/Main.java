48e4decdee672898b400740712bfc57f524fbd67
teoaranda <157788932+teoaranda@users.noreply.github.com>
2024-05-15 12:17:33 +0200

    Update Main.java

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/Main.java
@@ -61,7 +61,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) { //GIT TEO
+    public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
 
         // Agregamos algunos libros de ejemplo
