c7fb59c1921297ad1ccf7b74c4b35aa3cb321730
alexJimenez <alex.jimenez.estudios@gmail.com>
2024-05-15 10:13:29 +0200

    Commit Bibilioteca y leerEntradaPorConsola()

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/objetos/Biblioteca.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/objetos/Biblioteca.java
@@ -1,5 +1,8 @@
 package objetos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Clase para gestionar la biblioteca
 public class Biblioteca {
     private ArrayList<Libro> librosDisponibles = new ArrayList<>();
@@ -7,6 +10,15 @@ public class Biblioteca {
 
     public void agregarLibro(Libro libro) {
         librosDisponibles.add(libro);
+    }
+
+
+
+    public void leerEntradaPorConsola() { //GIT ALEX
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nIntroduce tu nombre: ");
+        String nombreUsuario = scanner.nextLine();
+        System.out.println("¡Hola, " + nombreUsuario + "!");
     }
 
     
