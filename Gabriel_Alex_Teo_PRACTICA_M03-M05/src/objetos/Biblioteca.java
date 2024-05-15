fda946fcc5fed28d0ee37906a2be797eac302434
alexJimenez <alex.jimenez.estudios@gmail.com>
2024-05-15 11:53:49 +0200

    Update LeerEntradaporConsola()

--- Gabriel_Alex_Teo_PRACTICA_M03-M05/src/objetos/Biblioteca.java
+++ Gabriel_Alex_Teo_PRACTICA_M03-M05/src/objetos/Biblioteca.java
@@ -1,5 +1,9 @@
 package objetos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,7 +23,25 @@ public class Biblioteca {
         System.out.print("\nIntroduce tu nombre: ");
         String nombreUsuario = scanner.nextLine();
         System.out.println("¡Hola, " + nombreUsuario + "!");
-    }
+        
+        // Escritura de archivo de texto en una carpeta específica
+        try {
+            File carpeta = new File("prestamos");
+            if (!carpeta.exists()) {
+                carpeta.mkdir();
+            }
 
+            FileWriter archivoSalida = new FileWriter("prestamos/prestamos.txt", true);
+            BufferedWriter escritor = new BufferedWriter(archivoSalida);
+            escritor.newLine();
+            escritor.write("Usuario: " + nombreUsuario);
+            escritor.write(" - Libro prestado: " + libroSeleccionado.getTitulo());
+            escritor.close();
+            System.out.println("\nInformación de préstamo actualizada en prestamos/prestamos.txt");
+        } catch (IOException e) {
+            System.err.println("\nError al escribir el archivo: " + e.getMessage());
+        }
+    }
+    
     
 }
